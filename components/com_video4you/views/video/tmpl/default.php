7a88b56f52981c9f9a1d39f2bd4fba41c2e3cc13
Marcin <Marcin@WIN-83FEMUMTF9M>
2015-02-03 17:51:18 +0100

    videojs

--- components/com_video4you/views/video/tmpl/default.php
+++ components/com_video4you/views/video/tmpl/default.php
@@ -19,9 +19,9 @@ defined('_JEXEC') or die('Restricted access');
 	<?php if($this->item->image){ ?>
 		<p><strong>Image</strong>: <img src="images/com_video4you/<?php echo $this->item->image; ?>" /></p>
 	<?php } ?>
-	<p><strong>Video_480p</strong>: <?php echo $this->item->video480p; ?></p>
+<!--	<p><strong>Video_480p</strong>: <?php echo $this->item->video480p; ?></p>
 	<p><strong>Video_720p</strong>: <?php echo $this->item->video720p; ?></p>
-	<p><strong>Video_1080p</strong>: <?php echo $this->item->video1080p; ?></p>
+	<p><strong>Video_1080p</strong>: <?php echo $this->item->video1080p; ?></p>-->
 <!--                                        <script>
                     videojs.options.flash.swf = "video-js.swf";
                   </script>-->
@@ -29,11 +29,35 @@ defined('_JEXEC') or die('Restricted access');
               <video id="example_video_1" class="video-js vjs-default-skin" controls preload="none" width="640" height="264"
                   poster="images/com_video4you/<?php echo $this->item->image; ?>"
                   data-setup='{ "nativeControlsForTouch": false, "techOrder": ["html5","flash"], "plugins" : { "resolutionSelector" : { "default_res" : "480" } } }'>
-                <source src="http://video-js.zencoder.com/oceans-clip.mp4" type='video/mp4' />
-                <source src="http://video-js.zencoder.com/oceans-clip.webm" type='video/webm' />
-                <source src="http://video-js.zencoder.com/oceans-clip.ogv" type='video/ogg' />
-                <track kind="captions" src="demo.captions.vtt" srclang="en" label="English"></track><!-- Tracks need an ending tag thanks to IE9 -->
-                <track kind="subtitles" src="demo.captions.vtt" srclang="en" label="English"></track><!-- Tracks need an ending tag thanks to IE9 -->
+                     <?php      
+                        if (($this->item->video720p == NULL) && ($this->item->video1080p == NULL))
+                         {
+                              echo'<source src="'.$this->item->video480p.'" type="video/mp4" data-res="480" />';
+                         }
+                              elseif (($this->item->video480p == NULL) && ($this->item->video1080p == NULL)) {
+                                      echo'<source src="'.$this->item->video720p.'" type="video/mp4" data-res="720" />';
+                         }
+                              elseif (($this->item->video480p == NULL) && ($this->item->video720p == NULL)){
+                                      echo'<source src="'.$Model->video_1080p.'" type="video/mp4" data-res="1080" />';
+                         }
+                              elseif ($this->item->video480p == NULL) {
+                                      echo'<source src="'.$this->item->video720p.'" type="video/mp4" data-res="720" />';
+                                      echo'<source src="'.$this->item->video1080p.'" type="video/mp4" data-res="1080" />';
+                         }
+                              elseif ($this->item->video720p == NULL) {
+                                      echo'<source src="'.$this->item->video480p.'" type="video/mp4" data-res="480" />';
+                                      echo'<source src="'.$this->item->video1080p.'" type="video/mp4" data-res="1080" />';
+                         }
+                              elseif ($this->item->video1080p == NULL) {
+                                      echo'<source src="'.$this->item->video480p.'" type="video/mp4" data-res="480" />';
+                                      echo'<source src="'.$this->item->video720p.'" type="video/mp4" data-res="720" />'; 
+                         }
+                         else {
+                              echo'<source src="'.$this->item->video480p.'" type="video/mp4" data-res="480" />';
+                              echo'<source src="'.$this->item->video720p.'" type="video/mp4" data-res="720" />';
+                              echo'<source src="'.$this->item->video1080p.'" type="video/mp4" data-res="1080" />';
+                        }
+        ?>
                 <p class="vjs-no-js">To view this video please enable JavaScript, and consider upgrading to a web browser that <a href="http://videojs.com/html5-video-support/" target="_blank">supports HTML5 video</a></p>
               </video>
 </div>
